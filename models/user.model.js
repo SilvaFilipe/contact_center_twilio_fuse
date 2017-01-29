89d567057f5b2cda59540ddeacc6f0d9b1774a5c
Brian Levine <beans@beanserver.net>
2017-01-29 01:21:34 -0800

    new user worker creation

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,12 @@
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
+const twilio = require('twilio')
+
+/* client for Twilio TaskRouter */
+const taskrouterClient = new twilio.TaskRouterClient(
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_AUTH_TOKEN,
+    process.env.TWILIO_WORKSPACE_SID)
 
 var userSchema = mongoose.Schema({
     firstName: {
@@ -23,6 +30,7 @@ var userSchema = mongoose.Schema({
         //min: [6, 'Password is too short.']
     },
     imageUrl: String,
+    workerSid: String,
     local: {
         email: {
             type: String
@@ -49,6 +57,46 @@ userSchema.methods.generateHash = function (password) {
 userSchema.methods.validPassword = function (password) {
     console.log(password, this.password);
     return bcrypt.compareSync(password, this.password);
+};
+
+userSchema.methods.syncWorker = function () {
+
+    var user = this;
+
+
+
+    taskrouterClient.workspace.workers.get({FriendlyName: this.id}, function (err, data) {
+        if (err) {
+            console.log('error finding worker to sync: ' + err)
+        } else {
+            console.log('data: ' + JSON.stringify(data.workers, null, 4));
+            console.log(data.workers.length );
+            if (data.workers.length == 0){
+                //create worker
+                var newWorkerData = {
+                    friendlyName: 'w' + user._id,
+                    attributes: JSON.stringify( { "contact_uri":"client:" + user._id, "channels":["phone","chat"],"team":"default", "email":user.email})
+                }
+                console.log('new user data: ' + JSON.stringify(newWorkerData, null, 4));
+                taskrouterClient.workspace.workers.create(newWorkerData)
+                    .then(function (newWorker) {
+                        console.log('created worker: ' + JSON.stringify(newWorker, null, 4));
+                        User.update({_id: user._id}, {
+                            workerSid: newWorker.sid
+                        }, function(err, affected, resp) {
+                            console.log(resp);
+                        })
+                    }).catch(function (err) {
+                    console.log('worker creation error: ' + JSON.stringify(err, null, 4));
+                })
+
+            } else {
+                //update worker
+                console.log('update worker');
+            }
+        }
+    });
+
 };
 
 module.exports = mongoose.model('User', userSchema);
