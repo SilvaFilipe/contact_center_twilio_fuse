d2bf7cda361b67d1e421826c2471aac3a69b9cb3
Diego Romero <romero.pozo2012@gmail.com>
2017-02-27 15:21:33 -0500

    replace es6 with compiled es5 service

--- src/app/services/user.service.js
+++ src/app/services/user.service.js
@@ -1,58 +1,54 @@
+'use strict';
+
 (function () {
   'use strict';
 
-  angular
-    .module('app.services')
-    .factory('UserService', UserService);
+  angular.module('app.services').factory('UserService', UserService);
 
   /** @ngInject */
   function UserService($http, $resource, $q) {
 
     var UserService = {
-      usersWithStars: function () {
+      usersWithStars: function usersWithStars() {
 
-        return UserService.$resource.query().$promise
-          .then(function (users) {
-            users = users.map(function (user) {
+        return UserService.$resource.query().$promise.then(function (users) {
+          users = users.map(function (user) {
 
-              user.starred = user.starredBy.findIndex(function (starredBy) {
-                  return starredBy.userId == req.user._id
-                }) > -1;
+            user.starred = user.starredBy.findIndex(function (starredBy) {
+                return starredBy.userId == req.user._id;
+              }) > -1;
 
-              return user;
-            });
-            return users;
-          })
+            return user;
+          });
+          return users;
+        });
       },
-      starUser: function (user) {
+      starUser: function starUser(user) {
         var defer = $q.defer();
 
         user.starred = !user.starred;
         user.id = user._id;
         user.$update(function (res) {
-          defer.resolve(res)
+          defer.resolve(res);
         });
 
         return defer.promise;
       },
 
-      $resource: $resource('/api/users/:id', {id: '@id'},
-        {
-          star: {
-            method: 'POST',
-            params: {
-              star: true
-            }
-          },
-          update: {
-            method: 'PUT' // this method issues a PUT request
+      $resource: $resource('/api/users/:id', { id: '@id' }, {
+        star: {
+          method: 'POST',
+          params: {
+            star: true
           }
-        })
+        },
+        update: {
+          method: 'PUT' // this method issues a PUT request
+        }
+      })
 
     };
 
-
     return UserService;
-
   }
 })();
