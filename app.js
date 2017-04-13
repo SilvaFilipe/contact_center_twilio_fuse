142b06edf9a519523d24e33127370c5b2978e136
Brian Levine <beans@beanserver.net>
2017-04-13 00:58:14 -0700

    change cors config

--- app.js
+++ app.js
@@ -29,7 +29,18 @@ var corsConfig = {
   "allowedHeaders":["Content-Type", "Authorization", "Origin", "Accept", "X-Request-With"]
 };
 
-app.use(cors(corsConfig));
+//app.use(cors(corsConfig));
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+});
 mongoose.connect(process.env.MONGO_URL);
 mongoose.connection.on('connected', function (err) {
     if (err) throw err;
