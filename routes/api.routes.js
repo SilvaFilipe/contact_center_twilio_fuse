56bb5a5e07a56ec167c3e59faf8287c10858eab9
Diego Romero <romero.pozo2012@gmail.com>
2017-05-30 08:48:36 -0500

    revert deleted routes

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -29,6 +29,7 @@ var groups = require('../controllers/groups.js')
 var queues = require('../controllers/queues.js')
 var contacts = require('../controllers/contacts.js')
 var dids = require('../controllers/dids.js')
+var resetPwdController = require('../controllers/resetPassword.js')
 //var s3 = require('../controllers/s3.js')
 
 module.exports = function(app, acl, multer){
@@ -94,6 +95,11 @@ module.exports = function(app, acl, multer){
 
     router.route('/messaging-adapter/inbound').post(messagingAdapter.inbound)
     router.route('/messaging-adapter/outbound').post(messagingAdapter.outbound)
+
+    //reset password api
+    router.route('/reset/forgot').post(resetPwdController.forgot);
+    router.route('/reset/:token').get(resetPwdController.validateResetToken);
+    router.route('/reset/:token').post(resetPwdController.resetPassword);
 
     //Users api
     router.route('/users/me').get(users.me);
