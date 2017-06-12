0c8a48922c98545a3150f5d00f6837b6059d9aaf
Dev <dev@Devs-Mac-2.local>
2017-06-12 01:38:26 -0400

    set focuse on input text after modal is opend

--- src/app/main/workspace/dialogs/keypad-dialog.controller.js
+++ src/app/main/workspace/dialogs/keypad-dialog.controller.js
@@ -6,7 +6,7 @@
     .controller('KeypadDialogController', KeypadDialogController);
 
   /** @ngInject */
-  function KeypadDialogController($scope, $rootScope, $mdDialog) {
+  function KeypadDialogController($scope, $timeout, $rootScope, $mdDialog) {
     var vm = this;
     $scope.hide = function() {
       $mdDialog.hide();
@@ -19,7 +19,7 @@
       if($rootScope.connection){
         $rootScope.connection.sendDigits(vm.numbers.charAt(vm.numbers.length-1));
       }
-      $('.inputKeypad').focus();
+      angular.element('.inputKeypad').focus();
     };
 
     vm.keyDowned = function (keyEvent) {
@@ -29,6 +29,9 @@
         }
       }
     };
+    $timeout(function () {
+      angular.element('.inputKeypad').focus();
+    }, 1000);
   }
 
 })();
