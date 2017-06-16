0d5450ec67e9bde3ee12ec1bb0a0fb83d8741223
Brian Levine <beans@beanserver.net>
2017-06-15 19:35:02 -0700

    cors fix for safari

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Origin', req.headers.origin);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Origin');
   if ('OPTIONS' == req.method) {
     res.send(200);
   } else {
