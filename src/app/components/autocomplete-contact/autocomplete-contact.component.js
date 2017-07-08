ca31191aaaed1f231dc5eb69ccfa34f579f23b1b
Dev <dev@Devs-Mac-2.local>
2017-07-08 12:46:41 -0400

    admin group autocomplete behavior change

--- src/app/components/autocomplete-contact/autocomplete-contact.component.js
+++ src/app/components/autocomplete-contact/autocomplete-contact.component.js
@@ -10,18 +10,34 @@ angular.module('app.components')
 /** @ngInject */
 function AutocompleteContactController($log, $rootScope, UserService, GroupService) {
   var $ctrl = this;
+  var items = [];
 
   $ctrl.queryModel = function () {
-    return UserService.queryExcludeGroupUsers($ctrl.group._id, $ctrl.searchText)
+    return UserService.queryExcludeGroupUsers($ctrl.group._id, $ctrl.searchText).then(function (res) {
+      items = res.filter(function(val) {
+        for( var i=0; i < $ctrl.group.users.length; i++ ){
+          if( $ctrl.group.users[i]._id === val._id ) {
+            return false;
+          }
+        }
+        return true;
+      });
+      return items;
+    }, function (err) {
+      console.log(err);
+    });
   };
 
   $ctrl.addToGroup = function (instance) {
-    if(!instance) return;
-    console.log($ctrl.group, instance);
+    if (!instance) return;
+    if (!angular.isDefined($ctrl.group.users)) {
+      $ctrl.group.users = [];
+    }
     if(!GroupService.isUserInGroup($ctrl.group, instance)){
       $ctrl.group.users.push(instance);
-      GroupService.update($ctrl.group._id, $ctrl.group);
     }
+    $ctrl.selectedItem = '';
+    $('#groupUserInput').trigger('blur');
   };
 
   $log.log('AutocompleteContactController load');
